917eb76e5a1f8087f8696b2ac8aee190da977f30
Aliona990220 <aliona.havela@gmail.com>
2021-08-15 12:38:53 +0200

    Tasks for yesterday by shafa

--- src/main/java/ShafaTestURL.java
+++ src/main/java/ShafaTestURL.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,62 +11,40 @@ public class ShafaTestURL {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://shafa.ua/women/nizhnee-bele-i-kupalniki/trusiki/52655023-komplekt-besshovnyh-trusikov");
-        WebElement element = driver.findElement(By.xpath("//div[@class='js-buy-button']//button"));
-        element.click();
-        String strUrl = driver.getCurrentUrl();
-        System.out.println(strUrl);
-
-        Assert.assertEquals(strUrl,"https://shafa.ua/login?next=/checkout/52655023s");
-        List<WebElement> elements = driver.findElements(By.xpath("//div"));
-        for (WebElement el: elements) {
-            System.out.println(el);
-        }
-        System.out.println(elements.size() > 10 );
-        driver.close();
-
-
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
-        WebDriver driver2 = new ChromeDriver();
         driver.get("https://shafa.ua/");
-        WebElement element2 = driver2.findElement((By.xpath("//div[@class='b-section__container']/ul")));
-        System.out.println(element2);
-        driver.close();
+//        WebElement element = driver.findElement((By.xpath("//div[@class='b-section__container']/h2")));
+//        String text =element.getText();
+//        System.out.println(text);
+//
+//        WebElement element1 = driver.findElements((By.xpath("//div[@class='b-section b-section_bg_brands']//h2"))).get(0);
+//        String text1 =element1.getText();
+//        System.out.println(text1);
 
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
-        driver.get("https://shafa.ua/");
-        System.out.println();
-        driver.close();
+        List <WebElement> element6 = driver.findElements((By.xpath("//div[@class='_1Oikv8416VOZKJriT08h']//a")));
+         Integer size = element6.size();
+            System.out.println(size);
 
-        Assert.assertEquals(strUrl,"https://shafa.ua/login?next=/checkout/52655023");
-        List<WebElement> elements2 = driver.findElements(By.xpath("//div[@class='b-section__container']"));
-        for (WebElement el1: elements2) {
-            System.out.println(el1);
-        }
-        System.out.println(elements2.size() > 10 );
-        driver.close();
+//        WebElement element3 = driver.findElement((By.xpath("//div[@class='b-block__promo']")));
+//        String text3 =element3.getText();
+//        System.out.println(text3);
 
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
-        WebDriver driver4 = new ChromeDriver();
-        driver.get("https://shafa.ua/");
-        WebElement element4 = driver4.findElement((By.xpath("//div[@class='js-suggestion-item-block']")));
-        System.out.println(element4);
-        driver.close();
+//        WebElement element4 = driver.findElements((By.xpath("//div[@class='DfLiXtdKirLRjXqzIZ7j _9FyXaaDBJB6Q-Kz8GIsD']"))).get(0);
+//        String text4 =element4.getText();
+//        System.out.println(text4);
 
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
-        WebDriver driver5 = new ChromeDriver();
-        driver.get("https://shafa.ua/");
-        WebElement element5 = driver5.findElement((By.xpath("//a[text()='Рекомендуем вам']")));
-        System.out.println(element5);
-        driver.close();
+//        WebElement element5 = driver.findElements((By.xpath("//div[@class='_3FwHm0lcQOk3fn7nVlWL']//a"))).get(1);
+//        String text5 =element5.getText();
+//        System.out.println(text5);
 
-        Assert.assertEquals(strUrl,"https://shafa.ua/login?next=/checkout/52655023");
-        List<WebElement> elements6 = driver.findElements(By.xpath("//div[@class='_1OWA_j74GKb-L50vQxC3']"));
-        for (WebElement el2: elements6) {
-            System.out.println(el2);
-        }
-        System.out.println(elements6.size() > 10 );
-        driver.close();
+//        WebElement element7 = driver.findElements((By.xpath("//div[@class='b-block']/a"))).get(0);
+//        String text7 =element7.getText();
+//        System.out.println(text7);
+
+        List <WebElement> element8 = driver.findElements((By.xpath("//div[@class='b-section__container']/ul")));
+        Integer size8 = element8.size();
+        System.out.println(size8);
+
+
 
 
 
