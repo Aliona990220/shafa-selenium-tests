49ebf3b5b0feb24f32e23b9a0bf932ab6dd25e7b
Aliona990220 <aliona.havela@gmail.com>
2021-08-17 14:59:59 +0200

    Tasks new by shafa.ua/uk

--- src/main/java/ShafaTestURL.java
+++ src/main/java/ShafaTestURL.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
@@ -12,39 +13,53 @@ public class ShafaTestURL {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://shafa.ua/");
-        WebElement element = driver.findElement((By.xpath("//div[@class='b-section__container']/h2")));
-        String text =element.getText();
-        System.out.println(text);
+//        WebElement element = driver.findElement((By.xpath("//div[@class='b-section__container']/h2")));
+//        String text =element.getText();
+//        System.out.println(text);
+//
+//        WebElement element1 = driver.findElements((By.xpath("//div[@class='b-section b-section_bg_brands']//h2"))).get(0);
+//        String text1 =element1.getText();
+//        System.out.println(text1);
+//
+//        List <WebElement> element6 = driver.findElements((By.xpath("//div[@class='_1Oikv8416VOZKJriT08h']//a")));
+//         Integer size = element6.size();
+//            System.out.println(size);
+//
+//        WebElement element3 = driver.findElement((By.xpath("//div[@class='b-block__promo']")));
+//        String text3 =element3.getText();
+//        System.out.println(text3);
+//
+//        WebElement element4 = driver.findElements((By.xpath("//div[@class='DfLiXtdKirLRjXqzIZ7j _9FyXaaDBJB6Q-Kz8GIsD']"))).get(0);
+//        String text4 =element4.getText();
+//        System.out.println(text4);
+//
+//        WebElement element5 = driver.findElements((By.xpath("//div[@class='_3FwHm0lcQOk3fn7nVlWL']//a"))).get(1);
+//        String text5 =element5.getText();
+//        System.out.println(text5);
+//
+//        WebElement element7 = driver.findElements((By.xpath("//div[@class='b-block']/a"))).get(0);
+//        String text7 =element7.getText();
+//        System.out.println(text7);
+//
+//        List <WebElement> element8 = driver.findElements((By.xpath("//div[@class='b-section__container']/ul")));
+//        Integer size8 = element8.size();
+//        System.out.println(size8);
 
-        WebElement element1 = driver.findElements((By.xpath("//div[@class='b-section b-section_bg_brands']//h2"))).get(0);
-        String text1 =element1.getText();
-        System.out.println(text1);
 
-        List <WebElement> element6 = driver.findElements((By.xpath("//div[@class='_1Oikv8416VOZKJriT08h']//a")));
-         Integer size = element6.size();
-            System.out.println(size);
+        String currentPage=driver.getCurrentUrl();
+        WebElement element9 = driver.findElements((By.xpath("//div[@class='_3FwHm0lcQOk3fn7nVlWL']//a"))).get(1);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element9).clickAndHold().release().build().perform();
+        String newPage = driver.getCurrentUrl();
+        if(currentPage.equals(newPage))
+        {
+            System.out.println("On Same Page");
 
-        WebElement element3 = driver.findElement((By.xpath("//div[@class='b-block__promo']")));
-        String text3 =element3.getText();
-        System.out.println(text3);
-
-        WebElement element4 = driver.findElements((By.xpath("//div[@class='DfLiXtdKirLRjXqzIZ7j _9FyXaaDBJB6Q-Kz8GIsD']"))).get(0);
-        String text4 =element4.getText();
-        System.out.println(text4);
-
-        WebElement element5 = driver.findElements((By.xpath("//div[@class='_3FwHm0lcQOk3fn7nVlWL']//a"))).get(1);
-        String text5 =element5.getText();
-        System.out.println(text5);
-
-        WebElement element7 = driver.findElements((By.xpath("//div[@class='b-block']/a"))).get(0);
-        String text7 =element7.getText();
-        System.out.println(text7);
-
-        List <WebElement> element8 = driver.findElements((By.xpath("//div[@class='b-section__container']/ul")));
-        Integer size8 = element8.size();
-        System.out.println(size8);
-
-
+        }
+        else
+        {
+            System.out.println("Ok");
+        }
 
 
 
