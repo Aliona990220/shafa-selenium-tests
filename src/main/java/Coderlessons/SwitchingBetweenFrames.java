db85d40bc970f54c3f2aa161be901ac67ea41fcf
Aliona990220 <aliona.havela@gmail.com>
2021-08-18 17:59:10 +0200

    Switching between frames.

--- src/main/java/Coderlessons/SwitchingBetweenFrames.java
+++ src/main/java/Coderlessons/SwitchingBetweenFrames.java
@@ -1,14 +1,16 @@
 package Coderlessons;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CloseAndExitEheWindowsBrowser {
+public class SwitchingBetweenFrames {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.popuptest.com/popuptest2.html");
-//        driver.close();
-        driver.quit();
+        driver.get("http://demo.guru99.com/selenium/deprecated.html");
+        driver.switchTo().frame("classFrame");
+        driver.findElement(By.linkText("Deprecated")).click();
+        driver.close();
     }
 }
