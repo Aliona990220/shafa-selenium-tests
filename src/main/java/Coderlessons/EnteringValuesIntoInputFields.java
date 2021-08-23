d33fb45c21e7a2ee5e292c5d3d7a8d57e82848c8
Aliona990220 <aliona.havela@gmail.com>
2021-08-23 20:26:02 +0200

    Entering values ​​into input fields.

---
+++ src/main/java/Coderlessons/EnteringValuesIntoInputFields.java
@@ -0,0 +1,23 @@
+package Coderlessons;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EnteringValuesIntoInputFields {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://demo.guru99.com/test/login.html");
+        WebElement email =  driver.findElements(By.xpath("//div[@class='container']//input")).get(0);
+        Thread.sleep(3000);
+        email.click();
+        email.sendKeys("abcd@gmail.com");
+        WebElement password =  driver.findElements(By.xpath("//div[@class='container']//input")).get(1);
+        password.click();
+        password.sendKeys("123456abcdftry");
+
+    }
+}
