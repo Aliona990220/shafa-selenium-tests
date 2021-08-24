6241da92b968e37ff45ff01aeaf465e8e574d05e
Aliona990220 <aliona.havela@gmail.com>
2021-08-24 17:43:44 +0200

    Login to the site.

--- src/main/java/Coderlessons/EnteringValuesIntoInputFields.java
+++ src/main/java/Coderlessons/EnteringValuesIntoInputFields.java
@@ -24,7 +24,7 @@ public class EnteringValuesIntoInputFields {
 //        login.click();
         //Або команда:
         login.submit();
-        
+
         driver.close();
 
 
---
+++ src/main/java/shafaSeleniumTests/LoginToTheSite.java
@@ -0,0 +1,35 @@
+package shafaSeleniumTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginToTheSite {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://shafa.ua/uk/");
+        WebElement login =  driver.findElements(By.xpath("//div[@class='_1C8R7Ed8PZh8800Q5y1A']//a")).get(2);
+        Thread.sleep(5000);
+        login.click();
+        Thread.sleep(3000);
+        WebElement email =  driver.findElements(By.xpath("//div[@class='_2ze0hXfd8Fj8W5fmqJC6']//input")).get(0);
+        email.click();
+        email.clear();
+        email.sendKeys("AlionaHavelia");
+
+        WebElement password =  driver.findElements(By.xpath("//div[@class='_2ze0hXfd8Fj8W5fmqJC6']//input")).get(1);
+        password.click();
+        email.clear();
+        password.sendKeys("wbDcq");
+        WebElement exits =  driver.findElement(By.xpath("//div[@class='_2ze0hXfd8Fj8W5fmqJC6']//button"));
+        exits.submit();
+
+//        driver.close();
+
+
+
+    }
+}
