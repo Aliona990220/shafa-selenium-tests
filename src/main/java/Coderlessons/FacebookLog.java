6f07b91ec5e39b3ed802254f87d6cc1a128a4e20
Aliona990220 <aliona.havela@gmail.com>
2021-08-26 18:18:18 +0200

    Login to the site facebook.

---
+++ src/main/java/Coderlessons/FacebookLog.java
@@ -0,0 +1,27 @@
+package Coderlessons;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookLog {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://m.facebook.com/login/?locale2=ru_RU");
+        Thread.sleep(3000);
+        WebElement email =  driver.findElements(By.xpath("//div[@class='_5rut']//input")).get(6);
+        email.click();
+        email.sendKeys("+380995682428");
+
+        WebElement password =  driver.findElements(By.xpath("//div[@class='_5rut']//input")).get(7);
+        password.click();
+        password.sendKeys("021120");
+        WebElement exit =  driver.findElement(By.xpath("//div[@class='_2pie']//button"));
+        exit.click();
+
+//        driver.close();
+    }
+}
