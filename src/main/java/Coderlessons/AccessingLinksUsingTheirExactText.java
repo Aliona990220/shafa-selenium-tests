e0e0fb32a1b45fe9d20a481615fe408af100118b
Aliona990220 <aliona.havela@gmail.com>
2021-10-18 20:31:39 +0200

    Accessing links using their exact text

--- src/main/java/Coderlessons/AccessingLinksUsingTheirExactText.java
+++ src/main/java/Coderlessons/AccessingLinksUsingTheirExactText.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class EleventhTask {
+public class AccessingLinksUsingTheirExactText {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://demo.guru99.com/test/link.html");
+        driver.get("http://demo.guru99.com/test/ajax.html");
         driver.findElement(By.linkText("click here")).click();
         System.out.println("title of page is: " + driver.getTitle());
-//        driver.quit();
+        driver.quit();
     }
+
 }
+
