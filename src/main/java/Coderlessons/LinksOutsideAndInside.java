3315b31a14459b8123f5b2188abd0efbbfa52246
Aliona990220 <aliona.havela@gmail.com>
2021-10-21 17:50:55 +0200

    Links outside and inside

--- src/main/java/Coderlessons/LinksOutsideAndInside.java
+++ src/main/java/Coderlessons/LinksOutsideAndInside.java
@@ -3,17 +3,19 @@ package Coderlessons;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class LinksOutsideAndInsideTheBlock {
+public class LinksOutsideAndInside {
     public static void main(String[] args) {
+        String baseUrl = "http://demo.guru99.com/test/block.html";
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrayver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://demo.guru99.com/test/radio.html");
+
+        driver.get(baseUrl);
         driver.findElement(By.partialLinkText("Inside")).click();
         System.out.println(driver.getTitle());
         driver.navigate().back();
         driver.findElement(By.partialLinkText("Outside")).click();
         System.out.println(driver.getTitle());
         driver.quit();
+
     }
 }
